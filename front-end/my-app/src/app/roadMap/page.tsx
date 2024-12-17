01c394e3937d58a0559493f563acdd6a0e75f4c1
Heitor <heitorfelipe37@gmail.com>
2024-12-17 13:37:38 -0300

    adding roadMap page route

---
+++ front-end/my-app/src/app/roadMap/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RoadMapPage = () => {
+    return (
+        <div className='justify-center'>
+            <p className='text-neutral-700 text-center'>
+                content
+            </p>
+        </div>
+    );
+}
+
+export default RoadMapPage; 
