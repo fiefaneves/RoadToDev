1356d7cd13f71c9b5765c4e4dd9b4f305c348404
JosiasNetto <josiascaldasnetto08@gmail.com>
2024-12-17 13:45:26 -0300

    Delete comments

--- back-end/server.js
+++ back-end/server.js
@@ -1,4 +1,3 @@
-import path  from 'path'; // Import path
 import express from 'express'; // Import express
 import cors from 'cors'; // Import cors
 import dotenv from 'dotenv';  // Import dotenv
@@ -12,15 +11,11 @@ const app = express(); // Create express app
 
 app.use(cors()); // Enable cors 
 
+app.use(express.json());
 // Middleware
 // Enable body parser
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Set static folder
-//app.use(express.static(path.join(__dirname, 'public')));
-
-//app.use('/openai', require('./src/routes/usersRoutes'));
 
 app.listen(port, () => { // Start the server
     console.log(`Server is running on port ${port}`); // Log a message
@@ -29,7 +24,7 @@ app.listen(port, () => { // Start the server
 app.post('/generate', async (req, res) => { // Create a route
     // Get the answer from the form and send it to the OpenAI API
     const { queryDescription } = req.body;
-    
+
     try {
         const roadQuery = await generate(queryDescription);
         res.json({response: roadQuery}); // Send the response
