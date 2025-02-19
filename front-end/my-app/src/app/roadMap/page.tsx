1085dfb6865dc65cd00170b82bc4357db3789fc5
JosiasNetto <josiascaldasnetto08@gmail.com>
2025-02-19 18:16:38 -0300

    fix error

--- front-end/my-app/src/app/roadMap/page.tsx
+++ front-end/my-app/src/app/roadMap/page.tsx
@@ -19,20 +19,19 @@ const RoadMapPage = () => {
       <div className="min-h-screen flex flex-col items-center justify-center py-8">
         <div className="bg-white rounded-lg shadow-lg p-8 sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/2">
           <h1 className="text-3xl font-semibold text-center text-gray-800 mb-6">Seu RoadMap Gerado</h1>
-          {isLoading ? (
-            <p className="text-gray-500 text-center px-4 py-6">Gerando roadmap, por favor aguarde...</p>
-          ) : (
-            <p className="text-gray-700 text-left px-4 py-6">
-              {roadmap
-               ? roadmap
-                      .split("\n")
-                      .map((paragraph : string, index : number) => (
-                        <p key={index} className="mb-6">{paragraph}</p>
-                      ))
-               
-               : "Nenhum roadmap disponível. Por favor, tente novamente."}
-            </p>
-          )}
+            <div className="text-gray-700 text-left px-4 py-6">
+                {isLoading ? (
+                    <p className="text-gray-500 text-center px-4 py-6">Gerando roadmap, por favor aguarde...</p>
+                ) : (
+                    roadmap
+                    ? roadmap
+                        .split("\n")
+                        .map((paragraph: string, index: number) => {
+                            return <p key={index} className="mb-6">{paragraph}</p>;
+                        })
+                    : "Nenhum roadmap disponível. Por favor, tente novamente."
+                )}
+            </div>
         </div>
       </div>
     );
