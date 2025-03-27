5c882898363eb785581f13af7148853dca149c6d
Heitor <heitorfelipe37@gmail.com>
2025-03-27 19:37:13 -0300

    fix(main): allowing cors

--- back-end/src/routes/usersRoutes.js
+++ back-end/src/routes/usersRoutes.js
@@ -6,7 +6,12 @@ import authMiddleware from '../middleware/authMiddleware.js';
 
 const routes = (app) => {
 
-    app.use(cors());
+    app.use(cors({
+      origin: [
+        "https://road-to-dev.vercel.app/",
+        "http://localhost:3000/"
+      ]
+    }));
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
 
