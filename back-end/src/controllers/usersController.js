9197a76bf52e8357c23f1553e2e1e8f84e76687c
JosiasNetto <josiascaldasnetto08@gmail.com>
2025-03-31 17:57:07 -0300

    Return fix split

--- back-end/src/controllers/usersController.js
+++ back-end/src/controllers/usersController.js
@@ -15,10 +15,7 @@ const UsersController = {
         
         try {
             const roadQuery = await generate(queryDescription);
-            let topics = roadQuery.split("\n\n");
-            if(topics.length() < 2){
-                topics = roadQuery.split("\n");
-            }
+            const topics = roadQuery.split("\n\n");
             const arrayTopics = [];
             for(let i = 0; i < topics.length; i++){
                 arrayTopics.push({ topic: topics[i], completed: false })
